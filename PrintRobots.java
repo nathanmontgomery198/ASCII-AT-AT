59ecc27f7bfd0b2fa11befc504d1bdb8880338aa
nathanmontgomery198 <61637328+nathanmontgomery198@users.noreply.github.com>
2020-05-12 17:37:51 +0100

    Add files via upload

---
+++ PrintRobots.java
@@ -0,0 +1,28 @@
+package Practical01;
+
+public class PrintRobots {
+
+    public static void main(String[] args) {
+        System.out.println("               ________");
+        System.out.println("          _,.-Y  |  |  Y-._");
+        System.out.println("      .-~\"   ||  |  |  |   \"-.");
+        System.out.println("      [ #    #      #     #    # ]  _____");
+        System.out.println("      L__  [] |..------|:   _[----I\" .-{\"-.");
+        System.out.println("     I___|  ..| l______|l_ [__L]_[I_/r(=}=-P");
+        System.out.println("    [L______L_[________]______j~  '-=c_]/=-^---");
+        System.out.println("     \\_I_j.--.\\==I|I==_/.--L_]");
+        System.out.println("       [_((==)[`-----\"](==)j");
+        System.out.println("          I--I\"~~\"\"\"~~\"I--I");
+        System.out.println("          |[]|         |[]|");
+        System.out.println("          l__j         l__j");
+        System.out.println("          |!!|         |!!|");
+        System.out.println("          |..|         |..|");
+        System.out.println("          ([])         ([])");
+        System.out.println("          ]--[         ]--[");
+        System.out.println("          [_L]         [_L] ");
+        System.out.println("         /|..|\\       /|..|\\");
+        System.out.println("        `=}--{='     `=}--{='");
+        System.out.println("       .-^--r-^-.   .-^--r-^-.");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+    }
+}
